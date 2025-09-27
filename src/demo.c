a01d148ba4ce6c2339e239850fb0cc66b6053683
antone <antone@DESKTOP-S7GIIN1.powerhub>
2025-09-27 14:19:05 +0100

    Upgraded the basic server to handle multiple connections.
    For now these are just echo-ing data back. Will figure out where to move
    from here soon

--- src/demo.c
+++ src/demo.c
@@ -2,104 +2,123 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <pthread.h>
 #include <arpa/inet.h>
 
 #define PORT 31338
+#define CONN_BACKLOG 10
 #define BUFFER_SIZE 1024
 
 
-void handle_connection(int socket_fd);
+void *handle_connection(void *sock);
+
 
 int main()
 {
-    // define stuff
-    int server_fd, new_socket;
-    struct sockaddr_in address;
-    int addrlen = sizeof(address);
+    int server_sock;
+    int *client_sock;
+    struct sockaddr_in server_addr;
+    struct sockaddr_in client_addr;
+    socklen_t client_addr_size = sizeof(client_addr);
 
-    // Create the socket fd
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    // create the socket
+    if ((server_sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
-        perror("Creating socket failed");
+        perror("Failed to create the server socket");
+        exit(-1);
+    }
+
+    // Server setup
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server_addr.sin_port = htons(PORT);
+
+    // Bind the server socket
+    if (bind(server_sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0)
+    {
+        perror("Failed to bind the server socket");
+        close(server_sock);
         exit(-2);
     }
 
-    // Make sure we can re-use the socket
-    int opt = 1;
-    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
-    // Bind socket to port
-    // Accept only connections from localhost
-    address.sin_family = AF_INET;
-    /* address.sin_addr.s_addr = INADDR_ANY; */
-    address.sin_addr.s_addr = inet_addr("127.0.0.1");
-    address.sin_port = htons(PORT);
-
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
+    // Start listening
+    if (listen(server_sock, CONN_BACKLOG) < 0)
     {
-        perror("Failed binding socket to port");
-        close(server_fd);
+        perror("Failed to setup socket for listening");
+        close(server_sock);
         exit(-3);
     }
 
-
-    if (listen(server_fd, 3) < 0)
-    {
-        perror("Failed to start listener on socket");
-        close(server_fd);
-        exit(-4);
-    }
-
-    const char *hello = "hello from server";
     printf("Started listening on port %d...\n", PORT);
 
     while (1)
     {
-        // Accept a client connection
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
-                                 (socklen_t *)&addrlen)) < 0) {
-            perror("accept failed");
-            close(server_fd);
-            exit(EXIT_FAILURE);
+        client_sock = malloc(sizeof(int));
+
+        if (client_sock == NULL)
+        {
+            perror("Memory allocation failed while preparing to accept socket connection");
+            continue;
         }
 
-        printf("Accepted connection from %s:%d\n",
-                inet_ntoa(address.sin_addr),
-                ntohs(address.sin_port));
+        // Accept the connection and get a new port from the kernel
+        *client_sock = accept(server_sock, (struct sockaddr *)&client_addr, &client_addr_size);
+        if (*client_sock < 0)
+        {
+            perror("Failed to accept incoming connection");
+            free(client_sock);
+            continue;
+        }
 
-        handle_connection(new_socket);
+        printf("Connection received from %s:%d\n",
+                inet_ntoa(client_addr.sin_addr),
+                ntohs(client_addr.sin_port));
+
+
+        // Create a new thread for each client
+        pthread_t tid;
+        if (pthread_create(&tid, NULL, handle_connection, client_sock) != 0)
+        {
+            perror("Error creating new thread to handle client connection");
+            close(*client_sock);
+            free(client_sock);
+        }
+        else
+        {
+            // Let the thread loose
+            pthread_detach(tid);
+        }
     }
 
-    // Clean up
-    close(new_socket);
-    close(server_fd);
-    free(buffer);
+    close(server_sock);
+    return 0;
 }
 
 
-void handle_connection(int socket_fd)
+
+
+
+void *handle_connection(void *sock)
 {
+    int client_sock = *(int *)sock;
+    free(sock);
 
-    char *buffer = malloc(BUFFER_SIZE);
-
-    // 0 the buffer
-    if (!buffer)
-    {
-        perror("Failed to allocate request buffer on the heap");
-        exit(-1);
-    }
+    char buffer[BUFFER_SIZE];
     memset(buffer, 0, BUFFER_SIZE);
+    ssize_t bytes_read;
 
 
-    // // Read data from client
-    // int valread = read(new_socket, buffer, BUFFER_SIZE - 1);
-    // if (valread > 0) {
-    //     buffer[valread] = '\0';  // null terminate the string
-    //     printf("Client says: %s\n", buffer);
-    // }
+    while ((bytes_read = recv(client_sock, buffer, BUFFER_SIZE -1, 0)) > 0)
+    {
+        buffer[bytes_read] = '\0';
+        printf("c: %s\n", buffer);
 
-    // // Send response to client
-    // send(new_socket, hello, strlen(hello), 0);
-    // printf("Hello message sent\n");
+        // just echo
+        send(client_sock, buffer, bytes_read, 0);
+    }
 
+    close(client_sock);
+    puts("Client disconnected\n");
+    return NULL;
 }
